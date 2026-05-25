29ed06dfbc0f8b80ec0b333176d738dbf58f7642
huy302 <anhhuy@gmail.com>
2026-05-25 14:25:54 -0500

    The Chaos Block.v2 100% COMPLETE!

---
+++ The Chaos Block.v2.py
@@ -0,0 +1,33 @@
+from bedrock.server import Server
+import asyncio
+import random
+
+app = Server()
+
+@app.server_event
+async def ready(ctx):
+    print("Python Bridge is Active! Move in game to trigger the build!")
+
+@app.game_event
+async def block_broken(ctx):
+    roll = random.randint(1,6)
+    if roll == 1:
+        await ctx.server.run("fill ~ ~2 ~ ~ ~7 ~ diamond_block")
+    elif roll == 2:
+        await ctx.server.run("summon zombie ~ ~ ~")
+        await ctx.server.run("summon zombie ~ ~ ~")
+        await ctx.server.run("summon zombie ~ ~ ~")
+        await ctx.server.run("summon zombie ~ ~ ~")
+        await ctx.server.run("summon zombie ~ ~ ~")
+        await ctx.server.run("summon zombie ~ ~ ~")
+        await ctx.server.run("summon zombie ~ ~ ~")
+        await ctx.server.run("summon zombie ~ ~ ~")
+        await ctx.server.run("summon zombie ~ ~ ~")
+        await ctx.server.run("summon zombie ~ ~ ~")
+    elif roll == 3:
+        await ctx.server.run("effect @s levitation 10 1 true")
+    elif roll == 4:
+        await ctx.server.run("effect @s slow_falling 10 255 true")
+
+if __name__ == "__main__":
+     app.start("localhost", 6666)
